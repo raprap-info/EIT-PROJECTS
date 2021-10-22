a28280e42300d1bd5fcc5a09b2aac5593304afcf
raprap-info <repiso.raffy.email@gmail.com>
2021-10-22 14:17:35 +0800

    adding this code to my git repo

---
+++ js/custome-js.js
@@ -0,0 +1,343 @@
+function cHorse(){
+    if(img1==1){
+    document.getElementById("img1").style.display="none";
+    document.getElementById("t1").textContent = "SHOW HORSE";
+      return img1 = 0;
+    } 
+    else{
+       document.getElementById("img1").style.display="inline";
+       document.getElementById("t1").textContent = "HIDE HORSE";
+      return img1=1;
+      
+    }
+ 
+ }
+
+ function cDog(){
+    if(img2==1){
+    document.getElementById("img2").style.display="none";
+    document.getElementById("t2").textContent = "SHOW DOG";
+      return img2 = 0;
+    } 
+    else{
+       document.getElementById("img2").style.display="inline";
+       document.getElementById("t2").textContent = "HIDE DOG";
+      return img2=1;
+      
+    }
+ 
+ }
+
+
+ 
+ function cCow1(){
+    if(img3==1){
+    document.getElementById("img3").style.display="none";
+    document.getElementById("t3").textContent = "SHOW COW1";
+      return img3 = 0;
+    } 
+    else{
+       document.getElementById("img3").style.display="inline";
+       document.getElementById("t3").textContent = "HIDE COW1";
+      return img3=1;
+      
+    }
+ 
+ }
+ 
+ function cCow2(){
+    if(img4==1){
+    document.getElementById("img4").style.display="none";
+    document.getElementById("t4").textContent = "SHOW COW2";
+      return img4 = 0;
+    } 
+    else{
+       document.getElementById("img4").style.display="inline";
+       document.getElementById("t4").textContent = "HIDE COW2";
+      return img4=1;
+      
+    }
+ 
+ }
+ 
+ function cCow3(){
+    if(img5==1){
+    document.getElementById("img5").style.display="none";
+    document.getElementById("t5").textContent = "SHOW COW3";
+      return img5 = 0;
+    } 
+    else{
+       document.getElementById("img5").style.display="inline";
+       document.getElementById("t5").textContent = "HIDE COW3";
+      return img5=1;
+      
+    }
+ 
+ }
+ 
+ function cMan1(){
+    if(img6==1){
+    document.getElementById("img6").style.display="none";
+    document.getElementById("t6").textContent = "SHOW MAN1";
+      return img6 = 0;
+    } 
+    else{
+       document.getElementById("img6").style.display="inline";
+       document.getElementById("t6").textContent = "HIDE MAN1";
+      return img6=1;
+      
+    }
+ 
+ }
+ 
+ function cMan2(){
+    if(img7==1){
+    document.getElementById("img7").style.display="none";
+    document.getElementById("t7").textContent = "SHOW MAN2";
+      return img7 = 0;
+    } 
+    else{
+       document.getElementById("img7").style.display="inline";
+       document.getElementById("t7").textContent = "HIDE MAN2";
+      return img7 =1;
+      
+    }
+ 
+ }
+ 
+ function cMan3(){
+    if(img8==1){
+    document.getElementById("img8").style.display="none";
+    document.getElementById("t8").textContent = "SHOW MAN3";
+      return img8 = 0;
+    } 
+    else{
+       document.getElementById("img8").style.display="inline";
+       document.getElementById("t8").textContent = "HIDE MAN3";
+      return img8 = 1;
+      
+    }
+ 
+ }
+ 
+ function cMan4(){
+    if(img9==1){
+    document.getElementById("img9").style.display="none";
+    document.getElementById("t9").textContent = "SHOW MAN4";
+      return img9 = 0;
+    } 
+    else{
+       document.getElementById("img9").style.display="inline";
+       document.getElementById("t9").textContent = "HIDE MAN4";
+      return img9 =1;
+      
+    }
+ 
+ }
+ 
+ function cMan5(){
+    if(img10==1){
+    document.getElementById("img10").style.display="none";
+    document.getElementById("t10").textContent = "SHOW MAN5";
+      return img10 = 0;
+    } 
+    else{
+       document.getElementById("img10").style.display="inline";
+       document.getElementById("t10").textContent = "HIDE MAN5";
+      return img10=1;
+      
+    }
+ 
+ }
+ 
+ function cMan6(){
+    if(img11==1){
+    document.getElementById("img11").style.display="none";
+    document.getElementById("t11").textContent = "SHOW MAN6";
+      return img11 = 0;
+    } 
+    else{
+       document.getElementById("img11").style.display="inline";
+       document.getElementById("t11").textContent = "HIDE MAN6";
+      return img11=1;
+      
+    }
+ 
+ }
+ 
+ function cMan7(){
+    if(img12==1){
+    document.getElementById("img12").style.display="none";
+    document.getElementById("t12").textContent = "SHOW MAN7";
+      return img12 = 0;
+    } 
+    else{
+       document.getElementById("img12").style.display="inline";
+       document.getElementById("t12").textContent = "HIDE MAN7";
+      return img12=1;
+      
+    }
+ 
+ }
+ 
+ function cWheat(){
+    if(img13==1){
+    document.getElementById("img13").style.display="none";
+    document.getElementById("t13").textContent = "SHOW WHEAT";
+      return img13 = 0;
+    } 
+    else{
+       document.getElementById("img13").style.display="inline";
+       document.getElementById("t13").textContent = "HIDE WHEAT";
+      return img13=1;
+      
+    }
+ 
+ }
+ 
+ function cChicks(){
+    if(img14==1){
+    document.getElementById("img14").style.display="none";
+    document.getElementById("t14").textContent = "SHOW CHICKS";
+    document.getElementById("t14").style.backgroundColor ="#f1f1f1;";
+    document.getElementById("t14").style.color ="black;";
+      return img14 = 0;
+    } 
+    else{
+       document.getElementById("img14").style.display="inline";
+       document.getElementById("t14").textContent = "HIDE CHICKS";
+       document.getElementById("t14").style.backgroundColor ="#04AA6D";
+       document.getElementById("t14").style.color ="#ffff";
+      return img14=1;
+      
+    }
+ 
+ }
+ 
+ function cSheep(){
+    if(img15==1){
+    document.getElementById("img15").style.display="none";
+    document.getElementById("t15").textContent = "SHOW SHEEP";
+    document.getElementById("t15").style.backgroundColor ="#f1f1f1;";
+    document.getElementById("t15").style.color ="black;";
+      return img15 = 0;
+    } 
+    else{
+       document.getElementById("img15").style.display="inline";
+       document.getElementById("t15").textContent = "HIDE SHEEP";
+       document.getElementById("t15").style.backgroundColor ="#04AA6D";
+       document.getElementById("t15").style.color ="#ffff";
+      return img15=1;
+      
+    }
+ 
+ }
+ 
+ function cRabbits(){
+    if(img16==1){
+    document.getElementById("img16").style.display="none";
+    document.getElementById("t16").textContent = "SHOW RABBITS";
+      return img16 = 0;
+    } 
+    else{
+       document.getElementById("img16").style.display="inline";
+       document.getElementById("t16").textContent = "HIDE RABBITS";
+      return img16=1;
+      
+    }
+ 
+ }
+ 
+ function cCat(){
+    if(img17==1){
+    document.getElementById("img17").style.display="none";
+    document.getElementById("t17").textContent = "SHOW CAT";
+      return img17 = 0;
+    } 
+    else{
+       document.getElementById("img17").style.display="inline";
+       document.getElementById("t17").textContent = "HIDE CAT";
+      return img17=1;
+      
+    }
+ 
+ }
+ 
+ function cCart(){
+    if(img18==1){
+    document.getElementById("img18").style.display="none";
+    document.getElementById("t18").textContent = "SHOW CART";
+      return img18 = 0;
+    } 
+    else{
+       document.getElementById("img18").style.display="inline";
+       document.getElementById("t18").textContent = "HIDE CART";
+      return img18=1;
+      
+    }
+ 
+ }
+ 
+ function cRooster(){
+    if(img19==1){
+    document.getElementById("img19").style.display="none";
+    document.getElementById("t19").textContent = "SHOW ROOSTER";
+      return img19 = 0;
+    } 
+    else{
+       document.getElementById("img19").style.display="inline";
+       document.getElementById("t19").textContent = "HIDE ROOSTER";
+      return img19=1;
+      
+    }
+ 
+ }
+ 
+ function cHen(){
+    if(img20==1){
+    document.getElementById("img20").style.display="none";
+    document.getElementById("t20").textContent = "SHOW HEN";
+      return img20 = 0;
+    } 
+    else{
+       document.getElementById("img20").style.display="inline";
+       document.getElementById("t20").textContent = "HIDE HEN";
+      return img20=1;
+      
+    }
+ 
+ }
+ 
+ function cDog(){
+    if(img2==1){
+    document.getElementById("img2").style.display="none";
+    document.getElementById("t2").textContent = "SHOW DOG";
+      return img2 = 0;
+    } 
+    else{
+       document.getElementById("img2").style.display="inline";
+       document.getElementById("t2").textContent = "HIDE DOG";
+      return img2=1;
+      
+    }
+ 
+ }
+ 
+ function cDog(){
+    if(img2==1){
+    document.getElementById("img2").style.display="none";
+    document.getElementById("t2").textContent = "SHOW DOG";
+      return img2 = 0;
+    } 
+    else{
+       document.getElementById("img2").style.display="inline";
+       document.getElementById("t2").textContent = "HIDE DOG";
+      return img2=1;
+      
+    }
+ 
+ }
+
+ function back(){
+   location.replace("/");
+ }
